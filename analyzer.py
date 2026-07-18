4d0d4cf63304a9825208ef896fe9b31fbca9f3ae
nada.. <naadaa242@gmail.com>
2026-07-18 23:34:39 +0300

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,11 @@ import pandas as pd
 # قراءة البيانات من ملف خارجي
 df = pd.read_csv('logs.csv')
 
-# التحليل
+# تحليل المحاولات الفاشلة
 failed_attempts = df[df['Status'] == 'Failed']
+
+print("--- تقرير المحاولات الفاشلة ---")
+print(failed_attempts)
+
+print("\n--- تكرار المحاولات لكل عنوان IP ---")
 print(failed_attempts['IP_Address'].value_counts())
